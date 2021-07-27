6a08c98807170d8c558da8dff0edecd004eaea03
Yoshito Matsumoto <y_matsu@ga2.so-net.ne.jp>
2021-07-27 09:00:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Data/EntitySampleAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntitySampleApp.Models;
+
+namespace EntitySampleApp.Data
+{
+    public class EntitySampleAppContext : DbContext
+    {
+        public EntitySampleAppContext (DbContextOptions<EntitySampleAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntitySampleApp.Models.Person> Person { get; set; }
+    }
+}
